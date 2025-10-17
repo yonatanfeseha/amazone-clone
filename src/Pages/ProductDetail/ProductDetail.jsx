7426000ec55ecda8f0a4c883198b09fd7a0afb77
yonatanfeseha <122829231+yonatanfeseha@users.noreply.github.com>
2025-10-17 12:54:10 +0300

    loader

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -4,11 +4,14 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { productUrl } from "../Api/endPoints";
 import ProductCard from "../../Components/Product/ProductCard";
+import Loader from "../../Components/Loader/Loader";
 
 function ProductDetail() {
   const { productId } = useParams();
   const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     axios
       .get(`${productUrl}products/${productId}`)
       .then((res) => {
@@ -16,12 +19,11 @@ function ProductDetail() {
       })
       .catch((error) => {
         console.error("Error fetching product details:", error);
+        setLoading(false);
       });
   }, []);
   return (
-    <Layout>
-      <ProductCard product={product} />
-    </Layout>
+    <Layout>{loading ? <Loader /> : <ProductCard product={product} />}</Layout>
   );
 }
 
