7182e246132840c664d9fe7b1d52f4616661fb41
yonatanfeseha <122829231+yonatanfeseha@users.noreply.github.com>
2025-11-17 12:06:40 +0300

    orders page init

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Layout from "../../Components/Layout/Layout";
-
+import db from "../../Utility/firebase";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
+import classes from "./Orders.module.css";
 function Orders() {
-  return <Layout>orders</Layout>;
+  const [{ user }, dispatch] = useContext(DataContext);
+  useEffect(() => {
+    if (user) {
+      db.collection("user")
+        .doc(user.uid)
+        .collection("orders", "desc")
+        .onSnapShot((SnapShot) => {
+          setOrders(
+            SnapShot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          );
+        });
+    } else {
+    }
+  }, []);
+
+  return (
+    <Layout>
+      <section className={classes.container}>
+        <div className={classes.order_container}>
+          <h2>Your orders</h2>
+          <div></div>
+        </div>
+      </section>
+    </Layout>
+  );
 }
 
 export default Orders;
