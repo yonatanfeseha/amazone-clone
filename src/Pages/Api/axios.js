9a5da36a1310f5a1b4073446d8f32a670608e94c
yonatanfeseha <122829231+yonatanfeseha@users.noreply.github.com>
2025-11-19 22:02:30 +0300

    base url

--- src/Pages/Api/axios.js
+++ src/Pages/Api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: " http://localhost:3000",
+  // baseURL: " http://localhost:3000",
+  baseURL: " https://amazone-api-r7t4.onrender.com/",
 });
 export { axiosInstance };
